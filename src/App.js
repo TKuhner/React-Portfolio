fce96305b86cc3b1ba3a86d68c6db1d98744f04f
TKuhner <trevorkuhner29@gmail.com>
2023-07-29 14:15:24 -0500

    added files and started App setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
 import Navbar from './components/Navbar';
 
+
+
 function App() {
+
   return (
     <div className="app">
+      <Router>
       <Navbar />
+        <Routes>
+          
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,18 @@
+
+
+
+
 const Navbar = () => {
     return (
-        <div className="navbar flex justify-between">
+        <nav>
             <div>
                 TK
             </div>
-            <div className="flex text-align">
-                <div>
-                    Home
-                </div>
-                <div>
-                    Skills
-                </div>
-                <div>
-                    Projects
-                </div>
-                <div>
-                    Contact
-                </div>
-                <div>
-                    <a>Resume</a>
-                </div>
+            <div>
+                
             </div>
-            </div>
-    )
+        </nav>
 
-}
+    )
+};
 export default Navbar;
