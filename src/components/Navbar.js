ce1e66151233076cd1f0b0ab1e4f3b4420b10b53
TKuhner <trevorkuhner29@gmail.com>
2023-07-29 10:38:34 -0500

    installed tailwind and created nav bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+const Navbar = () => {
+    return (
+        <div className="navbar flex justify-between">
+            <div>
+                TK
+            </div>
+            <div className="flex text-align">
+                <div>
+                    Home
+                </div>
+                <div>
+                    Skills
+                </div>
+                <div>
+                    Projects
+                </div>
+                <div>
+                    Contact
+                </div>
+                <div>
+                    <a>Resume</a>
+                </div>
+            </div>
+            </div>
+    )
+
+}
+export default Navbar;
