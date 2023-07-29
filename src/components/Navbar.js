1de073c95d2f7da8da170f94c96ed810ae25956c
TKuhner <trevorkuhner29@gmail.com>
2023-07-29 15:53:32 -0500

    navbar base styling good, need to build pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,15 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className='bg-red z-40 w-full fixed top-0 py-6'>
-            <div className="bg-red-500 justify-between">
-                <h4 className="flex">TK</h4>
-                <div className="flex">
-                    <Link to='/' className=''><a>About</a></Link>
-                    <Link to='/skills' className=''><a>Skills</a></Link>
-                    <Link to='/projects' className=''><a>Projects</a></Link>
-                    <Link to='/contact' className=''><a>Contact</a></Link>
-                    <a href="">Resume</a>
+        <nav className='m-0 bg-red z-40 w-full fixed top-0 py-6'>
+            <div className="flex items-center justify-between">
+                <h4 className="text-3xl font-bold p-8">TK</h4>
+                <div className="flex items-center p-2">
+                    <Link to='/' className=''><a className="p-2">About</a></Link>
+                    <Link to='/skills' className=''><a className="p-2">Skills</a></Link>
+                    <Link to='/projects' className=''><a className="p-2">Projects</a></Link>
+                    <Link to='/contact' className=''><a className="p-2">Contact</a></Link>
+                    <a href="" className="p-2">Resume</a>
                 </div>
             </div>
             
